8dde5d06e54f4198dd6728e4a6077fdd7946d94d
Edgy3ddy <114752473+Edgy3ddy@users.noreply.github.com>
2022-10-11 19:55:36 +0300

    Create zd2_tab.c

---
+++ LAB/zd2_tab.c
@@ -0,0 +1,29 @@
+//Obliczyc funkcja y = ax^2 znaczenia, gdy a - stale. x - zmieniase z x0 do xn, krokiem hx 2022-10-11
+#include<stdio.h>
+#include<math.h>
+
+int main (){
+	
+	int x,a,x0,xn,xh,i=0;
+	
+	printf("Wprowadz a\n");
+	scanf("%d",&a);
+	printf("Wprowadz x0\n");
+	scanf("%d",&x0);
+	printf("Wprowadz xn\n");
+	scanf("%d",&xn);
+	if (x0>xn || xn<0 ){
+		printf("x0 jest wieksze niz xn");
+		exit(0);
+	}
+	
+	printf("Wprowadz xh\n");
+	scanf("%d",&xh);
+	
+	for (x = x0; x <= xn; x=x+xh){
+	float k = a*x;
+	printf("%d. ax^2 = %0.1f, x = %d, \n",i++, pow(k, 2), x);
+	}
+	return 0;
+
+}
