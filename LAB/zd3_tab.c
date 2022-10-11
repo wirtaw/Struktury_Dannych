41668c630b297e3c41dd4d042d033891ac0713a6
Edgy3ddy <114752473+Edgy3ddy@users.noreply.github.com>
2022-10-11 21:09:34 +0300

    Update zd3_tab.c

--- LAB/zd3_tab.c
+++ LAB/zd3_tab.c
@@ -1,1 +1,30 @@
+//    Obliczyc funkcja y = ax^2 znaczenia, gdy a - stale, x jest wybierany z tablicy X(n) 2022-10-11
+#include<math.h>
+#include<stdio.h>
 
+
+int main (){
+
+        double x[] = { 1.0, 2.0, 3.0};
+        double a;
+        int size;
+        size = sizeof(x) / sizeof(x[0]);
+        printf("%d", size);
+
+        int n;
+        scanf("%d", &n);
+        if (n >= size){
+                printf("n > ilosc elementow w tablicy");
+                exit(1);
+        }
+        scanf("%lf", &a);
+
+
+
+
+        printf("wprowadz %f\n", a * x[n] * x[n]);
+
+
+        return 0;
+
+}
