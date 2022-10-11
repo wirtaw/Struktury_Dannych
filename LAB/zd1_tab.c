dbae2b7f00078824271cdf0d3f37b34838e56fcd
Edgy3ddy <114752473+Edgy3ddy@users.noreply.github.com>
2022-10-11 18:28:45 +0300

    Create zd1_tab.c

---
+++ LAB/zd1_tab.c
@@ -0,0 +1,22 @@
+//Obliczyc funkcja y = ax^2 znaczenia, gdy a ir x zmeniajan sie jednoczesnie: a z 1 krokiem 0.5, x - z 2 do 6 krokiem 2 200-10-11
+
+#include<stdio.h>
+#include<math.h>
+
+int func (float a, int x){
+	int i;
+	for (i = 0; i <= 3; i++){
+	float k = a*x;
+	printf("%0.1f\n", pow(k, 2));
+	a=a+0.5;
+	x=2+2;
+	}
+
+}
+
+
+int main (){
+	
+	func(1.0,2);
+
+}
